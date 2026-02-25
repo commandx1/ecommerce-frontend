01de85b527f0c6e7dda4c939587ec91bdfac9e7c
serhatbelen <serhat@onlay.ai>
2026-02-25 08:45:53 +0300

    Remove LegalBreadcrumb

--- src/app/legal/page.tsx
+++ src/app/legal/page.tsx
@@ -1,6 +1,5 @@
 import ContactSupport from "./components/ContactSupport"
 import LegalArchive from "./components/LegalArchive"
-import LegalBreadcrumb from "./components/LegalBreadcrumb"
 import LegalHero from "./components/LegalHero"
 import LegalPageContent from "./components/LegalPageContent"
 import QuickNavigationCards from "./components/QuickNavigationCards"
@@ -8,7 +7,6 @@ import QuickNavigationCards from "./components/QuickNavigationCards"
 export default function LegalPage() {
   return (
     <>
-      <LegalBreadcrumb />
       <LegalHero />
       <QuickNavigationCards />
       <div className="app-container mx-auto px-4 sm:px-6 lg:px-8 pb-16">
