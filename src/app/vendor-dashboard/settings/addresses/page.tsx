1d884ae5ff89c7a7e0186fc647bcdc7e9d596321
serhatbelen <serhatbelen7@gmail.com>
2026-08-22 13:36:08 +0300

    chore: remove VendorAddressesPage component to streamline vendor dashboard settings

--- src/app/vendor-dashboard/settings/addresses/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client"
-
-import AddressManagementShared from "@/components/dashboard-shared/AddressManagementShared"
-
-export default function VendorAddressesPage() {
-  return <AddressManagementShared />
-}
